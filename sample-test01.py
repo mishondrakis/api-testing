f6d8e4c4eff29bd3699f7a45b744ceb324b0cc2c
Mihail Hadzhiev <mihail.hadzhiev@rewardgateway.com>
2021-02-22 11:18:10 +0200

    second commit

---
+++ sample-test01.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+BASE_Url = "https://reqres.in"
+
+params = {'page': 2}
+
+responseOne = requests.get(BASE_Url + "/api/users", params=params)
+
+print(json.dumps(responseOne.json(), indent=4))
+resp = responseOne.json()
+import ipdb; ipdb.set_trace()
