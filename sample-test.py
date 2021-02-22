974937863ca7f75ee163a28b80d8b744075ad603
Mihail Hadzhiev <mihail.hadzhiev@rewardgateway.com>
2021-02-22 10:53:02 +0200

    second commit

--- sample-test.py
+++ sample-test.py
@@ -3,9 +3,8 @@ import json
 
 BASE_Url = "https://reqres.in"
 
+params = {'page': 2}
 
-
-
-responseOne = requests.get(BASE_Url + "/api/users/2")
+responseOne = requests.get(BASE_Url + "/api/users", params=params)
 
 print(json.dumps(responseOne.json(), indent=4))
